7b6056c6dd9c6dec955e1ade3d7cd2656ffe3633
Meer (0xJkn) <172110630+Meerata0@users.noreply.github.com>
2024-06-14 23:52:37 -0700

    Add files via upload

---
+++ UPMS.WindowsFormsApp/DataAccess/DAO/RoleDao.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using UPMS.WindowsFormsApp.Entity;
+
+namespace UPMS.WindowsFormsApp.DataAccess.DAO
+{
+    public class RoleDao
+    {
+        SqlConnection _connection = new SqlConnection(dbConnection.dbCon("upmsDB"));
+        RoleEntity re = new RoleEntity();
+
+        public DataTable GetALL()
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand("Select * From role_tb");
+            cmd.Connection = _connection;
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            _connection.Close();
+            return dt;
+        }
+        public DataTable GetAllActiveUserData()
+        {
+            _connection.Open();
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT role_tb.rid, role_tb.role_name, user_tb.is_active FROM role_tb INNER JOIN user_tb ON role_tb.rid=user_tb.role_id WHERE user_tb.is_active=1");
+                cmd.Connection = _connection;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            _connection.Close();
+            return dt;
+
+        }
+        public void GetSearch(string search, DataGridView dgv)
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM role_tb where concat(rid, role_name) LIKE '%" + search + "%' ");
+            cmd.Connection = _connection;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgv.DataSource = dt;
+            _connection.Close();
+        }
+
+        public bool Save(RoleEntity re)
+        {
+            _connection.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("insert into role_tb (role_name) values (@rolename)");
+                cmd.Connection = _connection;
+                cmd.Parameters.AddWithValue("@rolename", re.RoleName);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            _connection.Close();
+            return true;
+
+        }
+
+        public bool Edit(RoleEntity re)
+        {
+            _connection.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("update role_tb set role_name=@rolename where rid=@rid");
+                cmd.Connection = _connection;
+                cmd.Parameters.AddWithValue("@rid", re.RId);
+                cmd.Parameters.AddWithValue("@rolename", re.RoleName);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            _connection.Close();
+            return true;
+        }
+
+        public bool Delete(RoleEntity re)
+        {
+            _connection.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("delete from role_tb where rid=@rid");
+                cmd.Connection = _connection;
+                cmd.Parameters.AddWithValue("@rid", re.RId);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            _connection.Close();
+            return true;
+        }
+    }
+}
---
+++ UPMS.WindowsFormsApp/UI/PagesForm/RolePage/RolePageForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using UPMS.WindowsFormsApp.DataAccess.DAO;
+using UPMS.WindowsFormsApp.Entity;
+
+namespace UPMS.WindowsFormsApp.UI.PagesForm.RolePage
+{
+    public partial class RolePageForm : Form
+    {
+        RoleDao roleDao = new RoleDao();
+        RoleEntity roleEntity = new RoleEntity();
+
+        public RolePageForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void RoleGetAll()
+        {
+            roledgv.DataSource = roleDao.GetALL();
+        }
+        private void RolePageForm_Load(object sender, EventArgs e)
+        {
+            RoleGetAll();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            roleDao.GetSearch(txtSearch.Text, roledgv);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            RoleDetailsForm roleDetailsForm = new RoleDetailsForm();
+            roleDetailsForm.Text = "New Role Details";
+            roleDetailsForm.btnEdit.Visible = false;
+            roleDetailsForm.ShowDialog();
+            RoleGetAll();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            RoleDetailsForm roleDetailsForm = new RoleDetailsForm();
+            if (roledgv.SelectedRows.Count > 0)
+            {
+                roleDetailsForm.Text = "Edit Role Details";
+                roleDetailsForm.btnSave.Visible = false;
+                roleDetailsForm.txtId.Text = roledgv.SelectedRows[0].Cells[0].Value.ToString();
+                roleDetailsForm.txtRoleName.Text = roledgv.SelectedRows[0].Cells[1].Value.ToString();
+                roleDetailsForm.Show();
+            }
+            RoleGetAll();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            roleEntity.RId = Convert.ToInt32(roledgv.SelectedRows[0].Cells[0].Value.ToString());
+            roleEntity.RoleName = roledgv.SelectedRows[0].Cells[1].Value.ToString();
+            if (roledgv.SelectedRows.Count > 0)
+            {
+                bool delete = roleDao.Delete(roleEntity);
+                if (delete == true)
+                {
+                    MessageBox.Show("Data Deleted Successful");
+                }
+                else
+                {
+                    MessageBox.Show("Data Deleted Successful");
+                }
+            }
+            RoleGetAll();
+        }
+    }
+}
